04c7dfe76ae4c007f54892e3105a8c03020dcc97
Ertuğrul Emre Cihan <cihanertugrulemre368@gmail.com>
2021-02-27 01:45:41 +0300

    27.02.2021

---
+++ Business/Concrete/CarImageManager.cs
@@ -0,0 +1,55 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarImageManager : ICarImageService
+    {
+        ICarImageDal _CarImageDal;
+
+        public CarImageManager(ICarImageDal carImageDal)
+        {
+            _CarImageDal = carImageDal;
+        }
+
+        public IResult Add(CarImage carImage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult CheckIfImageLimitExceded(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Delete(CarImage carImage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<CarImage> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<CarImage>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<CarImage>> GetImagesByCarId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IResult Update(CarImage carImage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
